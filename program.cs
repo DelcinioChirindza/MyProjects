665b905f9bbc8406429fd12803601ac937862d5c
Delcinio_Chirindza <delchirindza@gmail>
2019-09-13 16:31:58 +0200

    Adicionando primeiro commit

---
+++ program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace vsCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduza o primeiro numero:");
+            int num= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduza o segundo nemero:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int soma = num+num2;
+            Console.WriteLine("A soma dos dois numeros = "+soma);
+            Console.ReadLine();
+        }
+    }
+}
+
